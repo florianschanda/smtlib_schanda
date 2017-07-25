8de87f88ed0bf3870578931460590ffb76715ee9
Florian Schanda <florian@schanda.org.uk>
2017-07-25 11:15:07 +0100

    Add small helper script.

---
+++ spark_2014/get_relevant_directories.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+##############################################################################
+##                                                                          ##
+##                            smtlib_schanda                                ##
+##                                                                          ##
+##              Copyright (C) 2017, Altran UK Limited                       ##
+##                                                                          ##
+##  This file is part of smtlib_schamda.                                    ##
+##                                                                          ##
+##  smtlib_schamda is free software: you can redistribute it and/or modify  ##
+##  it under the terms of the GNU General Public License as published by    ##
+##  the Free Software Foundation, either version 3 of the License, or       ##
+##  (at your option) any later version.                                     ##
+##                                                                          ##
+##  smtlib_schanda is distributed in the hope that it will be useful,       ##
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of          ##
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           ##
+##  GNU General Public License for more details.                            ##
+##                                                                          ##
+##  You should have received a copy of the GNU General Public License       ##
+##  along with smtlib_schanda. If not, see <http://www.gnu.org/licenses/>.  ##
+##                                                                          ##
+##############################################################################
+
+# This tiny script produces a list of directory that contain
+# benchmarks filed in AUFBVFPDTNIRA.
+
+import os
+
+dirs = set()
+
+for fn in os.listdir("AUFBVFPDTNIRA"):
+    if fn.endswith(".smt2"):
+        d, _ = fn.split("___", 1)
+        dirs.add(d)
+
+print "FLOAT_ONLY_BENCHMARKS = set(["
+for d in sorted(dirs):
+    print "    '%s'," % d
+print "])"
