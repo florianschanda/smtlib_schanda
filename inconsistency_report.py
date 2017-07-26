0911be0f55e31a34d8b58c0932bac870073ac4f9
Florian Schanda <florian@schanda.org.uk>
2017-07-26 11:12:24 +0100

    Fix duplicate reporting and write to file instead of stdout.

--- inconsistency_report.py
+++ inconsistency_report.py
@@ -36,62 +36,65 @@ cat_disagree_many = []
 cat_disagree_one  = []
 
 for group in GROUPS:
-    for data in solutions:
-        for bm in data["group_results"][group]:
-            bm_name = BENCHMARKS[bm]["name"]
-            verdicts = [(s["prover_kind"],
-                         s["group_results"][group][bm]["status"])
-                        for s in solutions]
+    for bm in solutions[0]["group_results"][group]:
+        bm_name = BENCHMARKS[bm]["name"]
+        verdicts = [(s["prover_kind"],
+                     s["group_results"][group][bm]["status"])
+                    for s in solutions]
 
-            if BENCHMARKS[bm]["status"] == "?":
-                # Check if we have different solutions
-                sat   = [v[0] for v in verdicts if v[1] == "s"]
-                unsat = [v[0] for v in verdicts if v[1] == "u"]
-                if len(sat) >= 1 and len(unsat) >= 1:
-                    cat_sat_and_unsat.append(
-                        "%s: different opinions: %s SAT; %s UNSAT" % \
-                        (bm_name,
-                         ", ".join(sat),
-                         ", ".join(unsat)))
-            elif BENCHMARKS[bm]["status"] in ("s", "u"):
-                # Check if all disagree
-                counter = {"s" : "u",
-                           "u" : "s"}[BENCHMARKS[bm]["status"]]
-                conform = [v[0]
-                           for v in verdicts
-                           if v[1] == BENCHMARKS[bm]["status"]]
-                rebel   = [v[0] for v in verdicts if v[1] == counter]
-                if len(conform) == 0 and len(rebel) == 0:
-                    cat_nobody.append("%s: unsolved" % bm_name)
-                elif len(conform) == 0 and len(rebel) == 1:
-                    cat_disagree_one.append(
-                        "%s: minority report from %s" %
-                        (bm_name,
-                         rebel[0]))
-                elif len(conform) == 0 and len(rebel) >= 1:
-                    cat_disagree_many.append(
-                        "%s: many (%s) disagree on %s expectation" %
-                        (bm_name,
-                         ", ".join(rebel),
-                         BENCHMARKS[bm]["status"]))
-            else:
-                assert False
+        if BENCHMARKS[bm]["status"] == "?":
+            # Check if we have different solutions
+            sat   = [v[0] for v in verdicts if v[1] == "s"]
+            unsat = [v[0] for v in verdicts if v[1] == "u"]
+            if len(sat) >= 1 and len(unsat) >= 1:
+                cat_sat_and_unsat.append(
+                    "%s: different opinions: %s SAT; %s UNSAT" % \
+                    (bm_name,
+                     ", ".join(sat),
+                     ", ".join(unsat)))
+        elif BENCHMARKS[bm]["status"] in ("s", "u"):
+            # Check if all disagree
+            counter = {"s" : "u",
+                       "u" : "s"}[BENCHMARKS[bm]["status"]]
+            conform = [v[0]
+                       for v in verdicts
+                       if v[1] == BENCHMARKS[bm]["status"]]
+            rebel   = [v[0] for v in verdicts if v[1] == counter]
+            if len(conform) == 0 and len(rebel) == 0:
+                cat_nobody.append("%s: unsolved" % bm_name)
+            elif len(conform) == 0 and len(rebel) == 1:
+                cat_disagree_one.append(
+                    "%s: minority report from %s" %
+                    (bm_name,
+                     rebel[0]))
+            elif len(conform) == 0 and len(rebel) >= 1:
+                cat_disagree_many.append(
+                    "%s: many (%s) disagree on %s expectation" %
+                    (bm_name,
+                     ", ".join(rebel),
+                     BENCHMARKS[bm]["status"]))
+        else:
+            assert False
 
-print "# Unsolved"
-for msg in cat_nobody:
-    print msg
+print "Writing report to `fishy.txt'"
+with open("fishy.txt", "w") as fd:
+    fd.write("# Inconsistency report\n")
+    if len(cat_nobody) > 0:
+        fd.write("\n## Unsolved\n")
+        for msg in cat_nobody:
+            fd.write(msg + "\n")
 
-print
-print "# Unknown with different opinions"
-for msg in cat_sat_and_unsat:
-    print msg
+    if len(cat_sat_and_unsat) > 0:
+        fd.write("\n## Unknown with different opinions\n")
+        for msg in cat_sat_and_unsat:
+            fd.write(msg + "\n")
 
-print
-print "# Minority report"
-for msg in cat_disagree_one:
-    print msg
+    if len(cat_disagree_one) > 0:
+        fd.write("\n## Minority report\n")
+        for msg in cat_disagree_one:
+            fd.write(msg + "\n")
 
-print
-print "# Everyone who solves disagrees with status"
-for msg in cat_disagree_many:
-    print msg
+    if len(cat_disagree_many) > 0:
+        fd.write("\n## Everyone who solves disagrees with status\n")
+        for msg in cat_disagree_many:
+            fd.write(msg + "\n")
