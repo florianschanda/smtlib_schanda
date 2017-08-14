4fcd0a2dfab2c15411615a0235fcde38d541d120
Florian Schanda <florian.schanda@altran.com>
2017-08-14 13:21:11 +0100

    Don't suppress smtlib version.
    Since we now have new-style datatypes, we must not supress the smtlib
    version anymore.

--- common.py
+++ common.py
@@ -101,8 +101,8 @@ class Benchmark(object):
                     status = tokens[2].strip(")")
                     assert status in ("unknown", "sat", "unsat")
                     self.expected = status
-                elif "set-info" in line and ":smt-lib-version" in line:
-                    pass
+                # elif "set-info" in line and ":smt-lib-version" in line:
+                #     pass
                 elif not keep_logic and "(set-logic" in line:
                     pass
                 else:
