4ba3f76baf46411c131382227daf08c565ec41eb
Florian Schanda <florian@schanda.org.uk>
2018-01-30 08:53:12 +0000

    Final data needs to include old/new on spark and maybe AE too.

--- run_all.py
+++ run_all.py
@@ -85,11 +85,11 @@ def main():
     if all_provers:
         # We pick a random (but consistent) version for the OldFP and
         # NoCBQI run for CVC4
-        # invocations.append(("oldfp", "cvc4_2018_01_27"))
+        invocations.append(("oldfp", "cvc4_2018_01_27"))
         # invocations.append(("nocbqi", "cvc4_2018_01_27"))
         invocations.append(("approx", "cvc4_2018_01_27"))
 
-        #invocations.append(("altergo", ALT_ERGO_VERSION))
+        invocations.append(("altergo", ALT_ERGO_VERSION))
         invocations.append(("altergo-fp", ALT_ERGO_FP_VERSION))
         invocations.append(("colibri", "colibri"))
         invocations.append(("mathsat", "mathsat"))
