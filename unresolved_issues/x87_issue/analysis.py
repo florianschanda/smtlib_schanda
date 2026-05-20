98cb181178455b1cd90cbbade1dee7ef21c1133f
Florian Schanda <florian@schanda.org.uk>
2026-05-20 12:46:11 +0200

    Figure out problem with x87

--- unresolved_issues/x87_issue/analysis.py
+++ unresolved_issues/x87_issue/analysis.py
@@ -4,22 +4,26 @@ from mpf.floats import *
 x = MPF(11, 53, 0x3ff0000000000000)
 y = MPF(11, 53, 0x5fefffffffffffff)
 r = fp_div(RM_RNE, x, y)
-print("%016x" % r.bv)
+print("expected       = %016x" % r.bv)
+print("actual (in 80) = 1ff0000000000000")
 
-x2 = fp_from_float(15, 79, RM_RNE, x)
-y2 = fp_from_float(15, 79, RM_RNE, y)
+# extended precision is (15, 64) and this is what happens in -mpc80
+x2 = fp_from_float(15, 64, RM_RNE, x)
+y2 = fp_from_float(15, 64, RM_RNE, y)
 r2 = fp_div(RM_RNE, x2, y2)
-r2_rounded = fp_from_float(11, 53, RM_RNE, r2)
-print("%016x" % r2_rounded.bv)
+r2_rounded = fp_from_float(11, 53, RM_RTZ, r2)
+print("double rounded = %016x" % r2_rounded.bv)
 
-# Double rounding does not explain the second wrong result
+# Second test vector gets the right result in 80 bit, but wrong in 53
 x = MPF(11, 53, 0x000fffffffffffff)
 y = MPF(11, 53, 0x3ff8c0493f45169b)
 r = fp_div(RM_RNE, x, y)
-print("%016x" % r.bv)
+print("expected       = %016x" % r.bv)
+print("actual (in 64) = 000a57ccb40cb71c")
 
-x2 = fp_from_float(15, 79, RM_RNE, x)
-y2 = fp_from_float(15, 79, RM_RNE, y)
+# What the dogshit unit does in -mpc64 is this:
+x2 = fp_from_float(15, 53, RM_RNE, x)
+y2 = fp_from_float(15, 53, RM_RNE, y)
 r2 = fp_div(RM_RNE, x2, y2)
 r2_rounded = fp_from_float(11, 53, RM_RNE, r2)
-print("%016x" % r2_rounded.bv)
+print("double rounded = %016x" % r2_rounded.bv)
